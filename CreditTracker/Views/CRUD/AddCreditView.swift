e7e51d3669b0d4fccf9c868afbec514c57ee161b
Shekar Balasubramaniam <chandrashekar9479@gmail.com>
2026-03-29 18:49:35 -0400

    commit 2 wo pbx changes

--- CreditTracker/Views/CRUD/AddCreditView.swift
+++ CreditTracker/Views/CRUD/AddCreditView.swift
@@ -43,13 +43,17 @@ struct AddCreditView: View {
                     }
                 }
 
-                Section("Preview") {
+                 Section("Preview") {
                     HStack(spacing: 12) {
                         ProgressRingView(
+                        ChunkyProgressRing(
                             fraction: 0,
                             startColor: Color(hex: card.gradientStartHex),
                             endColor: Color(hex: card.gradientEndHex),
                             lineWidth: 5,
+                            gradientStart: Color(hex: card.gradientStartHex),
+                            gradientEnd: Color(hex: card.gradientEndHex),
+                            strokeWidth: 6,
                             size: 44
                         )
                         VStack(alignment: .leading, spacing: 2) {
